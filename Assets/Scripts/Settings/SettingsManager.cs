422ab40d5dfd86759518e25692422f3f3ad94ea6
geoffrey.redon <geoffrey.redon@epita.fr>
2023-04-14 17:37:44 -0700

    Architecture DONE + Dictionary ADDED

---
+++ Assets/Scripts/Settings/SettingsManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Settings
+{
+    [Serializable] public class StringFloatDictionary : SerializableDictionary<string,float> { }
+    public class SettingsManager : MonoBehaviour
+    {
+        
+    }
+}
