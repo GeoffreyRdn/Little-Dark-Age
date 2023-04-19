9cec928cfd220c66c7c100cfe52a3ffebd91d2a7
geoffrey.redon <geoffrey.redon@epita.fr>
2023-04-18 18:11:49 -0700

    AI Detection ADDED

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,137 @@
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemies
+{
+    public class Enemy : MonoBehaviour
+    {
+        [Tag] [SerializeField] private string playerTag;
+
+        [BoxGroup("Mask")] [SerializeField] private LayerMask playerMask;
+        [BoxGroup("Mask")] [SerializeField] private LayerMask obstacleMask;
+        
+        private NavMeshAgent agent;
+        private GameObject[] players;
+
+        [BoxGroup("Debug")] [SerializeField] Transform target;
+
+        [BoxGroup("Settings")] [SerializeField] private float attackRange;
+        [BoxGroup("Settings")] [SerializeField] private float hearRange;
+        [BoxGroup("Settings")] [SerializeField] private float sightRange;
+        [BoxGroup("Settings")] [SerializeField] private float destinationRange;
+
+        [BoxGroup("Settings")] [SerializeField] private float sightAngle;
+
+        
+        private void Start()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            players = GameObject.FindGameObjectsWithTag(playerTag);
+        }
+        
+        private void Update()
+        {
+            UpdateTarget();
+            Chase();
+        }
+
+
+        private bool InDetectionRange(Transform player)
+        {
+            if (player == null) return false;
+            
+            Vector3 position = transform.position;
+            
+            // in hear range, the AI notice the player
+            if (Physics.CheckSphere(position, hearRange, playerMask))
+            {
+                Debug.Log("IN HEAR RANGE");
+                return true;
+            }
+
+            Vector3 playerDirection = (player.position - position).normalized;
+            
+            // check if the player is in sightRange + in FOV + no obstacle between player and AI
+            bool inRange = Physics.CheckSphere(position, sightRange, playerMask)
+                           && Mathf.Abs(Vector3.Angle(transform.forward, playerDirection)) < sightAngle / 2
+                           && !Physics.Raycast(position, playerDirection, sightRange, obstacleMask);
+
+            Debug.Log(inRange ? "IN SIGHT RANGE RANGE" : "NOT IN RANGE");
+
+            return inRange;
+        }
+        
+        
+        private void UpdateTarget()
+        {
+            // the AI is already following a player
+            // if (target != null) return;
+
+            (GameObject playerInRange, float distance) = (null, 0);
+            
+            foreach (GameObject player in players)
+            {
+                if (InDetectionRange(player.transform))
+                {
+                    var currentDistance = Vector3.Distance(player.transform.position, transform.position);
+                    
+                    // the AI will follow the closest player in detection range
+                    if (playerInRange == null || currentDistance < distance)
+                    {
+                        playerInRange = player;
+                        distance = currentDistance;
+                    } 
+                }
+            }
+
+            target = playerInRange != null ? playerInRange.transform : null;
+        }
+
+
+        private void Patrol()
+        {
+            return;
+        }
+
+        private void Chase()
+        {
+            if (target != null) agent.SetDestination(target.position);
+        }
+
+        #region DEBUG
+
+        private void OnDrawGizmos()
+        {
+            Vector3 position = transform.position;
+            float yAngle = transform.eulerAngles.y;
+
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(position, destinationRange);
+
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawWireSphere(position, sightRange);
+
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(position, hearRange);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(position, attackRange);
+
+            Gizmos.color = Color.blue;
+            float angleInDegL = yAngle + (sightAngle / 2);
+            float angleInDegR = yAngle - (sightAngle / 2);
+
+            Vector3 leftSide = new Vector3(Mathf.Sin(angleInDegL * Mathf.Deg2Rad), 0,
+                Mathf.Cos(angleInDegL * Mathf.Deg2Rad));
+            Vector3 rightSide = new Vector3(Mathf.Sin(angleInDegR * Mathf.Deg2Rad), 0,
+                Mathf.Cos(angleInDegR * Mathf.Deg2Rad));
+
+            Gizmos.DrawLine(position, position + leftSide * sightRange);
+            Gizmos.DrawLine(position, position + rightSide * sightRange);
+        }
+
+        #endregion
+
+    }
+}
