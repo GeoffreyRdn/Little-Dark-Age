be2b474c843dac5a023001af9b3a9090211178a2
geoffrey.redon <geoffrey.redon@epita.fr>
2023-04-23 16:41:37 -0700

    AI Attack + Animations DONE

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -1,27 +1,59 @@
+using System;
 using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.AI;
+using Random = UnityEngine.Random;
 
 namespace Enemies
 {
     public class Enemy : MonoBehaviour
     {
+        #region Variables
+
+        #region Editor
+        
         [Tag] [SerializeField] private string playerTag;
 
         [BoxGroup("Mask")] [SerializeField] private LayerMask playerMask;
         [BoxGroup("Mask")] [SerializeField] private LayerMask obstacleMask;
         [BoxGroup("Mask")] [SerializeField] private LayerMask wallMask;
         
-
-
         [BoxGroup("Settings")] [SerializeField] private float attackRange;
         [BoxGroup("Settings")] [SerializeField] private float hearRange;
         [BoxGroup("Settings")] [SerializeField] private float sightRange;
         [BoxGroup("Settings")] [SerializeField] private float destinationRange;
         [BoxGroup("Settings")] [SerializeField] private float abandonRange;
+        [BoxGroup("Settings")] [SerializeField] private float stopRange;
         [BoxGroup("Settings")] [SerializeField] private float sightAngle;
         
+        [BoxGroup("Settings")] [SerializeField] private float patrolSpeed;
+        [BoxGroup("Settings")] [SerializeField] private float chaseSpeed;
+        
         [BoxGroup("Debug")] [SerializeField] Transform target;
+        
+        #endregion
+
+        #region boolean
+
+        private bool isAttacking;
+        private bool isHit;
+
+        #endregion
+
+        #region Animations
+
+        private static readonly int IdleAnimation = Animator.StringToHash("idle");
+        private static readonly int WalkAnimation = Animator.StringToHash("walk");
+        private static readonly int RunAnimation = Animator.StringToHash("run");
+        private static readonly int AttackAnimation = Animator.StringToHash("attack");
+        private static readonly int HitAnimation = Animator.StringToHash("hit");
+        
+        private float lockedUntil;
+        private int currentState;
+
+        private Animator animator;
+
+        #endregion
 
         private NavMeshAgent agent;
         private GameObject[] players;
@@ -29,19 +61,69 @@ namespace Enemies
         private Vector3 destination;
         private bool hasDestination;
         
+        #endregion
+
+        #region Start - Update
+
         private void Start()
         {
+            animator = GetComponentInChildren<Animator>();
             agent = GetComponent<NavMeshAgent>();
             players = GameObject.FindGameObjectsWithTag(playerTag);
         }
         
         private void Update()
         {
-            UpdateTarget();
+            var state = GetState();
+            if (state != AttackAnimation)
+            {
+                UpdateTarget();
 
-            if (target != null) Chase();
-            else Patrol();
+                if (target != null)
+                {
+                    if (InAttackRange()) Attack();
+                    else Chase();
+                }
+
+                else Patrol();
+            }
+            
+            if (state == currentState) return;
+
+            animator.CrossFade(state, 0, 0);
+            currentState = state;
         }
+
+        #endregion
+
+        #region Methods Anim
+
+        private int GetState()
+        {
+            if (currentState == AttackAnimation) isAttacking = false;
+            if (currentState == HitAnimation) isHit = false;
+
+            if (Time.time < lockedUntil) return currentState;
+            
+            if (isAttacking) return LockState(AttackAnimation, 1.5f);
+            if (isHit) return LockState(HitAnimation, .7f);
+            
+            return agent.velocity.magnitude < .15f 
+                ? IdleAnimation 
+                : Math.Abs(agent.speed - patrolSpeed) < .1f 
+                    ? WalkAnimation 
+                    : RunAnimation;
+
+            int LockState(int s, float t)
+            {
+                lockedUntil = Time.time + t;
+                return s;
+            }
+        }
+        
+        #endregion
+
+        #region Check Range
 
         private bool InDetectionRange(Transform player)
         {
@@ -50,27 +132,33 @@ namespace Enemies
             Vector3 position = transform.position;
             
             // in hear range, the AI notice the player
-            if (Physics.CheckSphere(position, hearRange, playerMask))
-            {
-                Debug.Log("IN HEAR RANGE");
-                return true;
-            }
-
+            if (Physics.CheckSphere(position, hearRange, playerMask)) return true;
+            
             Vector3 playerDirection = (player.position - position).normalized;
             
             // check if the player is in sightRange + in FOV + no obstacle between player and AI
             bool inRange = Physics.CheckSphere(position, sightRange, playerMask)
                            && Mathf.Abs(Vector3.Angle(transform.forward, playerDirection)) < sightAngle / 2
                            && !Physics.Raycast(position, playerDirection, sightRange, obstacleMask);
-
-            Debug.Log(inRange ? "IN SIGHT RANGE RANGE" : "NOT IN RANGE");
-
+            
             return inRange;
         }
         
         private bool InFollowRange() 
             => Physics.CheckSphere(transform.position, abandonRange, playerMask);
         
+        private bool InAttackRange()
+        {
+            Vector3 position = transform.position;
+            return Physics.CheckSphere(position, attackRange, playerMask) &&
+                   (Physics.CheckSphere(position, stopRange, playerMask) ||
+                    !Physics.Raycast(position, (target.position - position).normalized, sightRange, obstacleMask));
+        }
+        
+        #endregion
+        
+        #region AI
+
         private void UpdateTarget()
         {
             // the AI is already following a player
@@ -95,8 +183,7 @@ namespace Enemies
 
             target = playerInRange != null ? playerInRange.transform : null;
         }
-
-
+        
         private void SearchForDestination()
         {
             Vector3 position = transform.position;
@@ -117,7 +204,8 @@ namespace Enemies
 
         private void Patrol()
         {
-            Debug.Log("PATROL");
+            agent.speed = patrolSpeed;
+            
             if (!hasDestination) SearchForDestination();
             if (hasDestination) agent.SetDestination(destination);
 
@@ -127,11 +215,28 @@ namespace Enemies
 
         private void Chase()
         {
-            Debug.Log("CHASING");
+            agent.speed = chaseSpeed;
             Vector3 enemyPosition = target.position;
             Vector3 targetPosition = new Vector3(enemyPosition.x, transform.position.y, enemyPosition.z);
             agent.SetDestination(targetPosition);
         }
+
+        private void Attack()
+        {
+            Vector3 agentPosition = transform.position;
+            Vector3 targetPosition = target.position;
+            
+            agent.SetDestination(agentPosition);
+            transform.LookAt(new Vector3(targetPosition.x, agentPosition.y, targetPosition.z));
+            isAttacking = true;
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.transform.CompareTag("Player")) isHit = true;
+        }
+        
+        #endregion
 
         #region DEBUG
 
@@ -142,6 +247,9 @@ namespace Enemies
 
             Gizmos.color = Color.green;
             Gizmos.DrawWireSphere(position, destinationRange);
+            
+            Gizmos.color = Color.grey;
+            Gizmos.DrawWireSphere(position, stopRange);
 
             Gizmos.color = Color.cyan;
             Gizmos.DrawWireSphere(position, sightRange);
@@ -169,6 +277,5 @@ namespace Enemies
         }
 
         #endregion
-
     }
 }
