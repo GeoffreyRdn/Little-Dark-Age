14d657d9bcaa4f1eef4faf9153c2997229ce7129
LeChienMagique <theo@prost.nom.fr>
2023-05-03 17:30:24 -0700

    add shop

--- Assets/Scripts/Inventory/InventorySlot.cs
+++ Assets/Scripts/Inventory/InventorySlot.cs
@@ -1,57 +1,10 @@
 using Items;
-using JetBrains.Annotations;
-using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
 
 namespace Inventory {
-	public class InventorySlot: MonoBehaviour, IPointerClickHandler {
-		[CanBeNull] public Item Item  = null;
-		public             int  Count = 0;
+	public class InventorySlot: ItemSlot {
 
-		public TextMeshProUGUI CountText;
-
-		protected Image  image;
-		public    Sprite DefaultSprite;
-
-		public bool HasItem = false;
-
-		public virtual void Start() {
-			image = GetComponent<Image>();
-
-			if (Item != null) {
-				ItemStack stack = new() {Item = Item, Count = Count};
-				HasItem        = true;
-				Item           = stack.Item;
-				image.sprite   = stack.Item.Icon;
-				CountText.text = Count.ToString();
-				CountText.gameObject.SetActive(true);
-			}
-			else {
-				image.sprite = DefaultSprite;
-
-				Color color = image.color;
-				color.a     = .5f;
-				image.color = color;
-			}
-		}
-
-		public void OnHover() {
-			if (!HasItem) {
-				return;
-			}
-			InventoryController.Instance.ShowItemDescription(this);
-		}
-
-		public void OnStopHover() {
-			if (!HasItem) {
-				return;
-			}
-			InventoryController.Instance.ClearItemDescription();
-		}
-
-		public void OnLeftClick() {
+		public override void OnLeftClick() {
 			if (HasItem && !InventoryController.IsHoldingItem) {
 				InventoryController.SetHeldItem(new ItemStack() {Item = Item, Count = Count});
 				RemoveItem();
@@ -64,7 +17,7 @@ namespace Inventory {
 			}
 		}
 
-		public void OnRightClick() {
+		public override void OnRightClick() {
 			if (!HasItem || Item!.ItemType != ItemType.Consumable) {
 				return;
 			}
@@ -76,54 +29,6 @@ namespace Inventory {
 			CountText.text = Count.ToString();
 			if (Count == 0) {
 				RemoveItem();
-			}
-		}
-
-		public void RemoveItem() {
-			HasItem      = false;
-			Count        = 0;
-			Item         = null;
-			image.sprite = DefaultSprite;
-			CountText.gameObject.SetActive(false);
-
-			Color color = image.color;
-			color.a     = .5f;
-			image.color = color;
-		}
-
-		public virtual void AddItem(ref ItemStack stack) {
-			if (HasItem && stack.Item.Id != Item.Id) {
-				return;
-			}
-
-			int addCount = stack.Count;
-			if (stack.Item.ItemType == ItemType.Consumable) {
-				ItemConsumable cons = (ItemConsumable) stack.Item;
-
-				if (Count + stack.Count > cons.MaxStack) {
-					addCount -= Count + stack.Count - cons.MaxStack;
-				}
-			}
-			stack.Count -= addCount;
-			Count       += addCount;
-
-			HasItem        = true;
-			Item           = stack.Item;
-			image.sprite   = stack.Item.Icon;
-			CountText.text = Count.ToString();
-			CountText.gameObject.SetActive(true);
-
-			Color color = image.color;
-			color.a     = 1f;
-			image.color = color;
-		}
-
-		public void OnPointerClick(PointerEventData eventData) {
-			if (eventData.button == PointerEventData.InputButton.Left) {
-				OnLeftClick();
-			}
-			if (eventData.button == PointerEventData.InputButton.Right) {
-				OnRightClick();
 			}
 		}
 	}
